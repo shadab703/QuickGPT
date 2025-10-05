13fd525cfcb8a4209f11f9f981fb0f157adf5a94
Shadab Siddiqui <shadabsiddiqui547@gmail.com>
2025-10-05 22:58:13 +0530

    Payment Status Complete

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,16 +1,16 @@
-import Stripe from "stripe"
-import Transaction from "../models/transaction.js";
-import User from "../models/user.js";
+import Stripe from 'stripe'
+import Transaction from '../models/transaction.js';
+import User from '../models/user.js';
+
 
 export const stripeWebhooks = async (request, response) => {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     const sig = request.headers["stripe-signature"];
     let event;
     try {
-        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
-
+        event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET)
     } catch (error) {
-        return response.status(400).send(`Webhook Error: ${error.message}`)
+        return response.status(400).send(`Webhook Error: ${error.message}`);
     }
     try {
         switch (event.type) {
@@ -25,21 +25,20 @@ export const stripeWebhooks = async (request, response) => {
                     const transaction = await Transaction.findOne({ _id: transactionId, isPaid: false });
                     // Update credits in user account
                     await User.updateOne({ _id: transaction.userId }, { $inc: { credits: transaction.credits } });
-                    // update credit payment status
                     transaction.isPaid = true;
                     await transaction.save();
                 } else {
-                    return response.json({ received: true, message: "Ignored event: Invalid app" });
+                    return response.json({ received: true, message: "Ignored Event: Invalid app" })
                 }
                 break;
             }
             default:
-                console.log("Unhandled Event type:", event.type)
+                console.log("Unhandled event type: ", event.type);
                 break;
         }
-        response.json({ received: true })
+        response.json({ received: true });
     } catch (error) {
-        console.error("Webhook processing Error:", error)
-        response.status(500).send("Internal Server Error")
+        console.error("Webhook processing error:", error);
+        response.status(500).send("Internal Server Error");
     }
 }
